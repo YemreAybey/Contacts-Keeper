0ed232839e437fdfb67b1f73467936d8d1e1766a
Yemre <aybey77@gmail.com>
2020-02-21 17:37:01 +0300

    deployment prepare

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import express from "express";
-import userRoutes from "./routes/users";
-import authRoutes from "./routes/auth";
-import contactsRoutes from "./routes/contacts";
-import connectDb from "./config/db";
+import express from 'express';
+import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
+import contactsRoutes from './routes/contacts';
+import connectDb from './config/db';
+import path from 'path';
 
 const app = express();
 
@@ -10,12 +11,15 @@ const PORT = process.env.PORT || 5000;
 
 connectDb();
 app.use(express.json({ extended: false }));
-app.use("/api/users", userRoutes);
-app.use("/api/contacts", contactsRoutes);
-app.use("/api/auth", authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/contacts', contactsRoutes);
+app.use('/api/auth', authRoutes);
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the Contact-Keeper API" });
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
